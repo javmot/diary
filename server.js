b203918c840f3fe71eadca5d3b86cb9b5b8126ed
Javier Mota <javier.mota.diaz@gmail.com>
2018-04-13 12:13:05 +0200

    Add static server and api proxy

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const request = require('request');
+
+const apiUrl = url => `https://api.abalin.net${url}`;
+const app = express();
+
+app.use('/api', (req, res) => {
+    const url = apiUrl(req.url);
+    req.pipe(request(url)).pipe(res);
+});
+app.use('/', express.static('source'));
+
+app.listen(process.env.PORT || 3000);
